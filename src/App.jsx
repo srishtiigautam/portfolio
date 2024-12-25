ab0f8e1f08e044790f0a59dbb22759c31d401bfe
srishtiigautam <95361333+srishtiigautam@users.noreply.github.com>
2024-12-25 13:05:11 +0530

    -Folder structure setup -Install react-router-dom -Format code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import './App.css'
-import Footer from './components/Footer.component'
-import Header from './components/Header.component'
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer.component";
+import Header from "./components/Header.component";
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Footer/>
+      <Header />
+      <Outlet />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/UiUxDesign.page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UiUxDesign = () => {
+  return <div>UiUxDesign</div>;
+};
+
+export default UiUxDesign;
