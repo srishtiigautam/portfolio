bfa9aee6880fb2f6eb0e1b2051c910938d2be3e1
srishtiigautam <95361333+srishtiigautam@users.noreply.github.com>
2024-12-26 14:00:24 +0530

    -Refactor project page

--- src/components/ProjectCard.component.jsx
+++ src/components/ProjectCard.component.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import Button from "./Button.component";
 
-const ProjectCard = ({name, imageUrl, description}) => {
+const ProjectCard = ({ name, imageUrl, description }) => {
   return (
     <div>
-      <div key={name} className="group h-64 w-64 [perspective:1000px]">
+      <div key={name} className="group h-72 w-64 [perspective:1000px]">
         <div className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
           {/* Front Face */}
           <div className="absolute inset-0 h-full w-full rounded-xl [backface-visibility:hidden]">
             {imageUrl && (
               <img
-                className="object-cover cursor-pointer object-left h-full w-full rounded-xl"
+                className="object-cover cursor-pointer object-center h-full w-full rounded-xl"
                 src={imageUrl}
                 alt={name}
               />
             )}
-            <p className="absolute inset-0 flex items-center justify-center text-2xl font-bold text-white bg-black/60 rounded-xl">
+            <p className="absolute inset-0 flex items-center justify-center text-center text-2xl font-bold text-white bg-black/60 rounded-xl">
               {name}
             </p>
           </div>
@@ -27,12 +27,7 @@ const ProjectCard = ({name, imageUrl, description}) => {
               <p className="text-md text-pretty text-center mb-2">
                 {description}
               </p>
-              <Button label="View Project"/>
-              {/* <a href="tel:555-555-5555" className="inline-flex">
-                <button className="my-2 bg-yellow-800 hover:bg-yellow-700 text-white font-bold py-2 px-4 w-auto rounded-full inline-flex items-center">
-                  <span>Schedule Service</span>
-                </button>
-              </a> */}
+              <Button label="View Project" />
             </div>
           </div>
         </div>
--- src/pages/Projects.page.jsx
+++ src/pages/Projects.page.jsx
@@ -1,6 +1,27 @@
 import React from "react";
 import ProjectCard from "../components/ProjectCard.component";
-import image from "../assets/image.png";
+import accumitt from "../assets/image.png";
+
+const projects = [
+  {
+    name: "Accumitt",
+    imageUrl: accumitt,
+    description:
+      "Accumitt is an AI powered accupressure gloves that targets specific points based on the symptoms.",
+  },
+  {
+    name: "Sahayak",
+    imageUrl: accumitt,
+    description:
+      "Sahayak is a public grievances redressal portal for villagers that connet people directly to local authorities.",
+  },
+  {
+    name: "Google Meet Summarizer",
+    imageUrl: accumitt,
+    description:
+      "This is an AI powered meeting summarizes chrome extension that summarizes you meetings into note format.",
+  },
+];
 
 const Projects = () => {
   return (
@@ -8,19 +29,23 @@ const Projects = () => {
       <div className="text-center mb-10">
         <h1 className="font-semibold text-4xl">Explore My Projects</h1>
         <p className="max-w-xl mt-5">
-          Dive into a collection of my work where technology meets design. Discover how I create seamless digital experiences.
+          Dive into a collection of my work where technology meets design.
+          Discover how I create seamless digital experiences.
         </p>
       </div>
       <div className="flex flex-col justify-center items-center">
         <h2 className="font-semibold text-3xl">Project Showcase</h2>
-        <p className="max-w-xl mt-5">Explore my diverse projects that blend technology and design.</p>
+        <p className="max-w-xl mt-5">
+          Explore my diverse projects that blend technology and design.
+        </p>
         <div className="max-w-3xl my-5 flex justify-center flex-wrap gap-5">
-          <ProjectCard name="Accumitt" imageUrl={image} description="Oil changes, tire rotations, and more to enhance performance and prevent future issues."/>
-          <ProjectCard name="Accumitt" imageUrl={image} description="Oil changes, tire rotations, and more to enhance performance and prevent future issues."/>
-          <ProjectCard name="Accumitt" imageUrl={image} description="Oil changes, tire rotations, and more to enhance performance and prevent future issues."/>
-          <ProjectCard name="Accumitt" imageUrl={image} description="Oil changes, tire rotations, and more to enhance performance and prevent future issues."/>
-          {/* <ProjectCard name="Accumitt" imageUrl={image} description="Oil changes, tire rotations, and more to enhance performance and prevent future issues."/> */}
-          {/* <ProjectCard name="Accumitt" imageUrl={image} description="Oil changes, tire rotations, and more to enhance performance and prevent future issues."/> */}
+          {projects.map((project) => (
+            <ProjectCard
+              name={project.name}
+              imageUrl={project.imageUrl}
+              description={project.description}
+            />
+          ))}
         </div>
       </div>
     </div>
