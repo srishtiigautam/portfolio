0ef49d81f21c0693da329290cfcb168f12477619
srishtiigautam <95361333+srishtiigautam@users.noreply.github.com>
2024-12-28 22:16:01 +0530

    -Mofidy Button component to take className as prop

--- src/components/Button.component.jsx
+++ src/components/Button.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({label}) => {
+const Button = ({label, className="bg-pink-600 text-white"}) => {
   return (
-    <button className='bg-pink-600 py-2 px-4 text-white rounded-md'>
+    <button className={`py-2 px-4 rounded-md ${className}`}>
       {label}
     </button>
   )
